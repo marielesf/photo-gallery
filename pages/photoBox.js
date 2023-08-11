215215b06a9679795dfa3efceaaa14b75ec49eeb
marielesf <mariele.fonttana@gmail.com>
2023-08-11 13:57:19 -0300

    initial project

---
+++ pages/photoBox.js
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function PhotoBox() {
+  return (
+    <div>
+      <Image src="/assets/ali-kazal-THPZYxsJ90A-unsplash.jpg" layout="fill" />
+    </div>
+  );
+}
